88b51323a9f7f08525bb3ab2fed159147e9bbb1c
MadCat <madcat@MadCatdeMacBook-Pro.local>
2025-12-19 14:58:08 +0800

    first commit

--- main.py
+++ main.py
@@ -1,24 +1,54 @@
-from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
+from astrbot.api.event import filter, AstrMessageEvent
+from astrbot.api.provider import ProviderRequest
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
+from astrbot.core.message.components import Image, Reply
 
-@register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0")
-class MyPlugin(Star):
+@register("DifyQuoteExt", "MadCat", "DifyQuoteExt for AstrBot", "1.0.0")
+class DifyQuoteExt(Star):
     def __init__(self, context: Context):
         super().__init__(context)
 
     async def initialize(self):
         """可选择实现异步的插件初始化方法，当实例化该插件类之后会自动调用该方法。"""
 
-    # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    @filter.command("helloworld")
-    async def helloworld(self, event: AstrMessageEvent):
-        """这是一个 hello world 指令""" # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
-        user_name = event.get_sender_name()
-        message_str = event.message_str # 用户发的纯文本消息字符串
-        message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
-        logger.info(message_chain)
-        yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
+    @filter.on_llm_request()
+    async def on_request(self, event: AstrMessageEvent, req: ProviderRequest): # 请注意有三个参数
+        quote = None
+        for comp in event.message_obj.message:
+            if isinstance(comp, Reply):
+                quote = comp
+                break
+        if quote:
+            sender_info = quote.sender_nickname or "[null]"
+            message_str = quote.message_str or "[Empty Text]"
+            req.system_prompt += (
+                f"\n[quote]\n"
+                "{\n"
+                f"  \"sender\": \"{sender_info}\"\n"
+                f"  \"message\": \"{message_str}\"\n"
+                # f"[quote_end]\n"
+            )
+            
+            # 处理引用图片
+            image_seg = None
+            if quote.chain:
+                for comp in quote.chain:
+                    if isinstance(comp, Image):
+                        image_seg = comp
+                        break
+            if image_seg:
+                try:
+                    req.image_urls.append(await image_seg.convert_to_file_path())
+                    req.system_prompt += (
+                        f"  \"image\": true\n"
+                    )
+                except BaseException as e:
+                    logger.error(f"处理引用图片失败: {e}")
+            req.system_prompt += (
+                "}\n"
+                f"[quote_end]\n"
+            )
 
     async def terminate(self):
         """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
